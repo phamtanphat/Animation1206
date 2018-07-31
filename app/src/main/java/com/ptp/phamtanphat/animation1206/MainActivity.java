4d9631b9cfc1a21a6b3c91e253e5c4cf75b01df3
thuviengithub0208 <khoaphp@yahoo.com>
2018-07-31 18:58:10 +0700

    "Animation"

--- app/src/main/java/com/ptp/phamtanphat/animation1206/MainActivity.java
+++ app/src/main/java/com/ptp/phamtanphat/animation1206/MainActivity.java
@@ -12,19 +12,28 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
 
 
-    ImageView imgAlpha;
+    ImageView imgAlpha,imgScale,imgRotate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         imgAlpha = findViewById(R.id.imageviewAlpha);
+        imgScale = findViewById(R.id.imageviewScale);
+        imgRotate = findViewById(R.id.imageviewRotate);
 
-        final Animation animationAlpha = AnimationUtils.loadAnimation(this,R.anim.custom_image_alpha);
-        imgAlpha.setOnClickListener(new View.OnClickListener() {
+        startAnimationFor(imgAlpha,R.anim.custom_image_alpha);
+        startAnimationFor(imgScale,R.anim.custom_image_scale);
+        startAnimationFor(imgRotate,R.anim.custom_image_rotate);
+    }
+    //X se tang tu trai sang phai
+    //Y se tang tu tren xuong duoi
+    public void startAnimationFor(ImageView imageView , int fileAnim){
+        final Animation animation = AnimationUtils.loadAnimation(this,fileAnim);
+        imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                view.startAnimation(animationAlpha);
+                view.startAnimation(animation);
             }
         });
     }
